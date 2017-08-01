7ae5ead8c8ed98541b77992dcac9bc09f4ed8cfa
nigelqiu <qiu.nigel@gmail.com>
2017-07-31 20:58:26 -0400

    Added getters/setters for Tutee and Tutor

--- assignment/src/general/Tutee.java
+++ assignment/src/general/Tutee.java
@@ -5,6 +5,23 @@ import java.util.ArrayList;
 public class Tutee extends Person{
 	
 	//List storing all possible tutors for the course being currently evaluated
-	private ArrayList<String> tutors = new ArrayList<String>();
+	private ArrayList<String> possibleTutors = new ArrayList<String>();
+	
+	//Methods for List possibleTutors
+	public void addPossibleTutor(String name) {
+		possibleTutors.add(name);
+	}
+	
+	public String getPossibleTutor(int index) {
+		return possibleTutors.get(index);
+	}
+	
+	public int possibleTutorsSize() {
+		return possibleTutors.size();
+	}
+	
+	public void clearPossibleTutors() {
+		possibleTutors.clear();
+	}
 
 }
--- assignment/src/general/Tutor.java
+++ assignment/src/general/Tutor.java
@@ -7,8 +7,22 @@ public class Tutor extends Person{
 	//Current number of sessions already assigned
 	private int curSessions = 0;
 	
+	//Methods for int maxSessions
 	public void setMaxSessions(int num) {
 		maxSessions = num;
+	}
+	
+	public int getMaxSessions() {
+		return maxSessions;
+	}
+	
+	//Methods for int curSessions
+	public void increaseCurSessions() {
+		curSessions++;
+	}
+	
+	public int getCurSessions() {
+		return curSessions;
 	}
 
 }
