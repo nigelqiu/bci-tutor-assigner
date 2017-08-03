ae54a54fa0c6173b5e5ee384bc66cb4c53161cb9
nigelqiu <qiu.nigel@gmail.com>
2017-08-03 19:58:55 -0400

    First addition to file writer

--- assignment/src/general/FileWriter.java
+++ assignment/src/general/FileWriter.java
@@ -1,7 +1,11 @@
 package general;
 
 import java.io.*;
+import java.nio.file.*;
 
 public class FileWriter {
 
+	//Path to assignments data file
+	private Path assignmentsFile = Paths.get("Assignments.txt");
+	
 }
