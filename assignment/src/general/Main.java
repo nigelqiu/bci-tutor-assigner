6a9c0408a143ec425d747fe82298beed2b2b9ab8
nigelqiu <qiu.nigel@gmail.com>
2017-08-03 20:17:01 -0400

    First addition to main

--- assignment/src/general/Main.java
+++ assignment/src/general/Main.java
@@ -1,5 +1,7 @@
 package general;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,6 +12,19 @@ public class Main {
 		Tutee[] tutees = read.initTuteeArr();
 		//Initializing and loading an Array of Tutors
 		Tutor[] tutors = read.initTutorArr();
+		//Boolean to run main while loop
+		Boolean run = true;
+		
+		//Precondition check to see if it is possible to run the main program
+		if (tutees.length == 0 || tutors.length == 0)
+			run = false;
+		
+		//Integer assigned a random number within possible tutees index values to randomly select a tutee
+		int targetTutee = ThreadLocalRandom.current().nextInt(0, tutees.length + 1);
+		//Main while loop
+		while (run) {
+			//[WIP]
+		}
 
 	}
 
