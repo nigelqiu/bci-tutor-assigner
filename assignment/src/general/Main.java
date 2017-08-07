23a4afe769877205f7d2eb834746ba135373f838
nigelqiu <qiu.nigel@gmail.com>
2017-08-06 21:24:10 -0400

    Second addition to main

--- assignment/src/general/Main.java
+++ assignment/src/general/Main.java
@@ -8,10 +8,8 @@ public class Main {
 		
 		//Creating instance of FileReader
 		FileReader read = new FileReader();
-		//Initializing and loading a master Array of Tutee
-		Tutee[] tuteesMaster = read.initTuteeArr();
-		//Copying tuteesMaster to another Tutee Array
-		Tutee[] tutees = tuteesMaster;
+		//Initializing and loading an Array of Tutee
+		Tutee[] tutees = read.initTuteeArr();
 		//Initializing and loading an Array of Tutors
 		Tutor[] tutors = read.initTutorArr();
 		//Creating instance of Compatibility
@@ -22,16 +20,18 @@ public class Main {
 		//Precondition check to see if it is possible to run the main program
 		if (tutees.length == 0 || tutors.length == 0)
 			run = false;
-		
 		//Integer assigned a random number within possible tutees index values to randomly select a tutee
-		int targetTutee = ThreadLocalRandom.current().nextInt(0, tuteesMaster.length + 1);
+		int targetTutee = ThreadLocalRandom.current().nextInt(0, tutees.length + 1);
 		//Main while loop
 		while (run) {
+			//For loop to check the compatibility between each tutor and the target tutee
 			for (int i = 0; i < tutors.length; i++) {
 				if (compatible.checkCompatible(tutees[targetTutee], tutors[i])) {
 					tutees[targetTutee].addPossibleTutor(tutors[i].getName());
 				}
 			}
+			
+			//
 		}
 
 	}
