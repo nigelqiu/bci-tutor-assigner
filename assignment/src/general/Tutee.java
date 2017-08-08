308899ebb5700143a5600a3010c6a27576eaf5c8
nigelqiu <qiu.nigel@gmail.com>
2017-08-08 17:42:38 -0400

    Update to tutee

--- assignment/src/general/Tutee.java
+++ assignment/src/general/Tutee.java
@@ -6,6 +6,8 @@ public class Tutee extends Person {
 
 	// List storing all possible tutors for the course being currently evaluated
 	private ArrayList<String> possibleTutors = new ArrayList<String>();
+	// Boolean marking whether the tutee is assigned
+	private boolean assigned;
 
 	// Methods for List possibleTutors
 	public void addPossibleTutor(String name) {
@@ -22,6 +24,15 @@ public class Tutee extends Person {
 
 	public void clearPossibleTutors() {
 		possibleTutors.clear();
+	}
+	
+	// Methods for Boolean assigned
+	public void setAssigned(boolean state) {
+		assigned = state;
+	}
+	
+	public boolean isAssigned() {
+		return assigned;
 	}
 
 }
