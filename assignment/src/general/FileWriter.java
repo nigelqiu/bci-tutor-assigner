a0fe764713576d516d429d5460a0359bd4e324c4
nigelqiu <qiu.nigel@gmail.com>
2017-08-14 20:47:50 -0400

    Second addition to file writer

--- assignment/src/general/FileWriter.java
+++ assignment/src/general/FileWriter.java
@@ -18,7 +18,78 @@ public class FileWriter {
 				input += line + '\n';
 			}
 			reader.close();
-			input += "Tutee (" + name + ") could not be assigned for target course (" + course +") \n\n";
+			// Preparing addition to file
+			input += "Tutee (" + name + ") could not be assigned for target course (" + course + ") \n\n";
+			// Initializing file output stream
+			FileOutputStream out = new FileOutputStream("Assignments.txt");
+			// Writing file
+			out.write(input.getBytes());
+			out.close();
+
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
+		}
+	}
+
+	public void startAssignment() {
+		// Initializing buffered reader
+		try (BufferedReader reader = Files.newBufferedReader(assignmentsFile)) {
+			String line;
+			String input = "";
+			while ((line = reader.readLine()) != null) {
+				// Storing entire file in input variable
+				input += line + '\n';
+			}
+			reader.close();
+			// Preparing addition to file
+			input += "Tutees (";
+			// Initializing file output stream
+			FileOutputStream out = new FileOutputStream("Assignments.txt");
+			// Writing file
+			out.write(input.getBytes());
+			out.close();
+
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
+		}
+	}
+
+	public void continueAssignment(String name) {
+		// Initializing buffered reader
+		try (BufferedReader reader = Files.newBufferedReader(assignmentsFile)) {
+			String line;
+			String input = "";
+			while ((line = reader.readLine()) != null) {
+				// Storing entire file in input variable
+				input += line + '\n';
+			}
+			reader.close();
+			// Preparing addition to file
+			input += " " + name + " ";
+			// Initializing file output stream
+			FileOutputStream out = new FileOutputStream("Assignments.txt");
+			// Writing file
+			out.write(input.getBytes());
+			out.close();
+
+		} catch (IOException x) {
+			System.err.format("IOException: %s%n", x);
+		}
+	}
+
+	public void endAssignment(String course, String time, String tutor) {
+		// Initializing buffered reader
+		try (BufferedReader reader = Files.newBufferedReader(assignmentsFile)) {
+			String line;
+			String input = "";
+			while ((line = reader.readLine()) != null) {
+				// Storing entire file in input variable
+				input += line + '\n';
+			}
+			reader.close();
+			// Preparing addition to file
+			input += ") are assigned for target course (" + course + ") at time slot (" + time + ") with tutor ("
+					+ tutor + ") \n\n";
 			// Initializing file output stream
 			FileOutputStream out = new FileOutputStream("Assignments.txt");
 			// Writing file
